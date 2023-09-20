2c568e316096cae97c8833a10060a9f666c669b9
SoTrxII <lo.lo.@lo.lo>
2023-09-20 12:07:04 +0200

    fix(encoder) : Adding proper event handling

--- pkg/uploader/uploader-api.go
+++ pkg/uploader/uploader-api.go
@@ -1,10 +1,19 @@
 package uploader
 
-import "time"
+import (
+	"fmt"
+	processing_common "processing-orchestrator/pkg/processing-common"
+	"time"
+)
 
 type UploadingService interface {
 	Upload(jobId, storageKey string, opt *VideoOpt) (*Video, error)
 }
+
+const (
+	// Topic the upload service is publishing into
+	s_Info = "upload-state"
+)
 
 type Visibility string
 
@@ -52,4 +61,43 @@ type Video struct {
 	ThumbnailUrl string `json:"thumbnailUrl,omitempty"`
 	// Url prefix necessary to watch the video. ie https://www.youtube.com/watch?v= for Youtube
 	WatchPrefix string `json:"watchPrefix"`
+}
+
+type UploadData struct {
+	/** Number of bytes uploaded so far */
+	Current int64 `json:"current"`
+	/** Size of the file to upload (bytes)*/
+	Total int64 `json:"total"`
+	/** Error message if any */
+	Message string `json:"message"`
+	/** Video link */
+	Link string `json:"link"`
+}
+type UploadEvent struct {
+	processing_common.ServiceEvent
+	Data UploadData `json:"data"`
+}
+
+func (e *UploadEvent) ToProgress() *processing_common.ServiceProgress {
+	pg := processing_common.ServiceProgress{
+		JobId:       e.JobId,
+		Step:        processing_common.StepEncoding,
+		CurrentItem: "video",
+		Error:       nil,
+		Progress:    "",
+	}
+	switch e.State {
+	case processing_common.Done:
+		// TODO : Add a data field to the progress
+		pg.Progress = "Done"
+	case processing_common.Error:
+		// TODO, Propagate error from upstream (not sent yet)
+		pg.Error = fmt.Errorf("Unkown error")
+	case processing_common.InProgress:
+		// TODO : Upsert ETA
+		pg.Progress = fmt.Sprintf("Uploaded %d bytes on total %d", e.Data.Current, e.Data.Total)
+	default:
+		pg.Error = fmt.Errorf("unsupported state %d", e.State)
+	}
+	return &pg
 }
--- pkg/uploader/uploader.go
+++ pkg/uploader/uploader.go
@@ -3,22 +3,79 @@ package uploader
 import (
 	"context"
 	"encoding/json"
+	"github.com/dapr/go-sdk/service/common"
 	"processing-orchestrator/internal/utils"
+	processing_common "processing-orchestrator/pkg/processing-common"
 )
 
 type Uploader struct {
-	invoker   utils.Invoker
-	component string
+	invoker         utils.Invoker
+	invokeComponent string
+	subComponent    string
+	// Events received from the Cooking Server
+	events chan UploadEvent
+	// Channel to send progress to
+	progressCh chan processing_common.Watchable
 }
 
-func NewUploader(invoker utils.Invoker, component string) *Uploader {
+func NewUploader(invoker utils.Invoker, invokeComponent, subComponent string, progressCh chan processing_common.Watchable) *Uploader {
 	return &Uploader{
-		invoker:   invoker,
-		component: component,
+		invoker:         invoker,
+		invokeComponent: invokeComponent,
+		subComponent:    subComponent,
+		events:          make(chan UploadEvent, 50),
+		progressCh:      progressCh,
+	}
+}
+func (c *Uploader) SubscribeTo(subServer utils.Subscriber) error {
+	err := subServer.AddTopicEventHandler(&common.Subscription{
+		PubsubName: c.subComponent,
+		Topic:      s_Info,
+	}, c.onInfo)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Uploader) onInfo(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
+	var evt UploadEvent
+	err = json.Unmarshal(e.RawData, &evt)
+	if err != nil {
+		return false, err
+	}
+	c.events <- evt
+	return false, nil
+}
+func (u *Uploader) handleEvents(ctx context.Context, jobId string) {
+Loop:
+	for {
+		select {
+		case evt := <-u.events:
+			if evt.JobId != jobId {
+				continue
+			}
+			// The "Done event" from upstream is not propagated
+			// for this one, as we will send our own
+			/// forged "Done event" when the upload is finished
+			if evt.State != processing_common.Done {
+				u.progressCh <- &evt
+			}
+
+			if evt.State == processing_common.Error || evt.State == processing_common.Done {
+				break Loop
+			}
+		case <-ctx.Done():
+		}
 	}
 }
 
 func (u *Uploader) Upload(jobId, storageKey string, opt *VideoOpt) (*Video, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	// So even if an error happen, the goroutine will be cleaned up
+	defer cancel()
+	go u.handleEvents(ctx, jobId)
+
 	uploadJob := UploadJob{
 		VideoOpt:   *opt,
 		StorageKey: storageKey,
@@ -28,7 +85,7 @@ func (u *Uploader) Upload(jobId, storageKey string, opt *VideoOpt) (*Video, erro
 	if err != nil {
 		return nil, err
 	}
-	res, err := u.invoker.InvokeMethodWithContent(context.Background(), u.component, "v1/videos", "POST", &utils.DataContent{
+	res, err := u.invoker.InvokeMethodWithContent(context.Background(), u.invokeComponent, "v1/videos", "POST", &utils.DataContent{
 		ContentType: "application/json",
 		Data:        bytes,
 	})
@@ -39,6 +96,16 @@ func (u *Uploader) Upload(jobId, storageKey string, opt *VideoOpt) (*Video, erro
 	err = json.Unmarshal(res, &video)
 	if err != nil {
 		return nil, err
+	}
+	// Custom done event with video properties
+	u.progressCh <- &UploadEvent{
+		ServiceEvent: processing_common.ServiceEvent{
+			JobId: jobId,
+			State: processing_common.Done,
+		},
+		Data: UploadData{
+			Link: video.WatchPrefix + video.Id,
+		},
 	}
 	return &video, nil
 }
